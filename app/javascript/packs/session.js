5424371866906fc3a01ce0d9f206e025c326aea1
berabjesus <64360974+Berabjesus@users.noreply.github.com>
2021-06-28 22:49:47 +0300

    Add login animation setup

---
+++ app/javascript/packs/session.js
@@ -0,0 +1,7 @@
+const toggle_button = document.getElementById("toggle")
+
+toggle_button.onclick = function () {
+  box = document.getElementById("dice")
+  box.classList.toggle('rotate')
+  console.log("sdfsd");
+}
